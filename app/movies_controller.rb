460bc4aa0140ee2a4e78b4a7d55f2d3a043e6d79
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-23 01:13:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,7 +78,6 @@ def can_update_using_update_method
   Movie.create(title: "Wat?")
   movie = Movie.find_by(title: "Wat?")
   movie.update(title: "Wat, huh?")
-  movie.save
 end
 
 def can_update_multiple_items_at_once

02bd8a3c28afa279fb41d912270126b3b369924a
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-23 00:54:23 +0000

    Automatically backed up by Learn

--- db/migrate/20200423005228_create_movies.rb
+++ db/migrate/20200423005228_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+    create_table :movies do |t|
+      t.title :string
+      t.release_date :integer
+      t.director :string
+      t.lead :string
+      t.in_theaters :boolean
   end
 end
